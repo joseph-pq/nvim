1dd2e1543f4c89abbf7c1cf386f4b04536912187
Joseph P. <joseph.pena.q@gmail.com>
2024-11-21 11:41:43 -0300

    💄(theme): set bg to transparent catppuccin-macchiato

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,6 +6,7 @@ function ColorMyPencils(color)
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 -- if env NVIM_BG_TRANSPARENT is True, then set bg to none
+vim.env.NVIM_BG_TRANSPARENT = "True"
 if vim.env.NVIM_BG_TRANSPARENT == "True" then
     vim.cmd('hi Normal ctermbg=NONE guibg=NONE')
     vim.cmd('hi NonText ctermbg=NONE guibg=NONE')
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,7 +2,7 @@ return {
     -- the colorscheme should be available when starting Neovim
     {
         "folke/tokyonight.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             -- load the colorscheme here
@@ -14,7 +14,18 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd([[colorscheme kanagawa]])
+            -- vim.cmd([[colorscheme kanagawa]])
         end,
     },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            trasnparent_background = true,
+        },
+        config = function()
+            vim.cmd.colorscheme("catppuccin-macchiato")
+        end,
+    }
 }
