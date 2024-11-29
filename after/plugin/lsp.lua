98e14117b3ad978707154b808125bfaec340611c
Joseph P. <joseph.pena.q@gmail.com>
2024-11-28 22:38:30 -0300

    🔧(lsp): setup typescript-language-server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,14 +3,14 @@ local lspconfig = require('lspconfig')
 
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count=1}) end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count=-1}) end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
@@ -21,7 +21,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'rust_analyzer', 'pyright', 'ruff', 'texlab'},
+  ensure_installed = { 'rust_analyzer', 'pyright', 'ruff', 'texlab', 'typescript-language-server' },
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
@@ -36,7 +36,7 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
@@ -44,13 +44,13 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
   sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
+    { name = 'path' },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'luasnip', keyword_length = 2 },
+    { name = 'buffer',  keyword_length = 3 },
   },
-  formatting = lsp_zero.cmp_format({details = false}),
+  formatting = lsp_zero.cmp_format({ details = false }),
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -66,8 +66,8 @@ cmp.setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
 lspconfig["pyright"].setup({
-  capabilities=capabilities,
-  settings={
+  capabilities = capabilities,
+  settings = {
     python = {
       analysis = {
         diagnosticMode = "workspace",
