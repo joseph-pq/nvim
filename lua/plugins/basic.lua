5a5a98d67bb6d4e2486988966291e50be05d73a2
Joseph P. <joseph.pena.q@gmail.com>
2024-12-05 17:54:10 -0300

    🐛 remove unexisting dependencies

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -65,17 +65,8 @@ return {
 		-- mode is `n` by default. For more advanced options, check the section on key mappings
 		keys = { "<C-a>", { "<C-x>", mode = "n" } },
 	},
-
-	-- local plugins need to be explicitly configured with dir
-	{ dir = "~/projects/secret.nvim" },
-
-	-- you can use a custom url to fetch a plugin
-	{ url = "git@github.com:folke/noice.nvim.git" },
-
 	-- local plugins can also be configured with the dev option.
-	-- This will use {config.dev.path}/noice.nvim/ instead of fetching it from GitHub
 	-- With the dev option, you can easily switch between the local and installed version of a plugin
-	{ "folke/noice.nvim",                         dev = true },
 	-- Highlight, edit, and navigate code
 	{ "nvim-treesitter/nvim-treesitter",          build = ":TSUpdate" },
 	{
