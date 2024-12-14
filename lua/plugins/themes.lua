4d7feb939843e1a97db9479815be08bd89c89c20
Joseph P. <joseph.pena.q@gmail.com>
2024-12-14 18:09:03 -0300

    🔧(theme): fix transparent mode

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,28 +4,18 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            -- vim.cmd([[colorscheme tokyonight]])
-        end,
     },
     {
         "rebelot/kanagawa.nvim",
         lazy = false,
         priority = 1000,
-        config = function()
-            -- vim.cmd([[colorscheme kanagawa]])
-        end,
     },
     {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
         opts = {
-            trasnparent_background = true,
+            transparent_background = true,
         },
-        config = function()
-            vim.cmd.colorscheme("catppuccin-macchiato")
-        end,
     }
 }
