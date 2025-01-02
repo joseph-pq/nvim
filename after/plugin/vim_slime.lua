ff980050178c9bb85f99c064442671b851a89565
Joseph P. <joseph.pena.q@gmail.com>
2025-01-02 11:20:48 -0300

    🔧(slime): remove <leader>s overlapping keymap

--- after/plugin/vim_slime.lua
+++ after/plugin/vim_slime.lua
@@ -5,7 +5,7 @@ vim.g.slime_dont_ask_default = 1
 vim.g.slime_bracketed_paste = 1
 
 vim.api.nvim_set_keymap('x', '<leader>s', '<Plug>SlimeRegionSend', {})
-vim.api.nvim_set_keymap('n', '<leader>s', '<Plug>SlimeMotionSend', {})
+-- vim.api.nvim_set_keymap('n', '<leader>s', '<Plug>SlimeMotionSend', {})
 vim.api.nvim_set_keymap('n', '<leader>ss', '<Plug>SlimeLineSend', {})
 vim.api.nvim_set_keymap('n', '<leader>sc', '<Plug>SlimeSendCell', {})
 vim.api.nvim_set_keymap('n', '<leader>sp', '<Plug>SlimeParagraphSend', {})
