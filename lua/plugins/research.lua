f572c79425efc344883c3d8d77e5d080a1f24e2d
Joseph P. <joseph.pena.q@gmail.com>
2025-01-13 11:21:36 -0300

    ✨ add research plugins including telescope-zotero.nvim

---
+++ lua/plugins/research.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"jmbuhr/telescope-zotero.nvim",
+		dependencies = {
+			{ 'kkharji/sqlite.lua' },
+		},
+		keys = {
+			{
+				"<leader>tz",
+				function()
+					require('telescope').extensions.zotero.zotero()
+				end,
+				desc = "Search Zotero",
+			},
+		},
+		config = function()
+			local telescope = require 'telescope'
+			-- other telescope setup
+			-- ...
+			telescope.load_extension 'zotero'
+		end
+	}
+}
