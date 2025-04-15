67eae2df2c5b3875361a9c31859c4fc101a3b1d4
Joseph P. <joseph.pena.q@gmail.com>
2025-04-15 18:25:36 -0300

    🐛 fix python executable in mypy

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -2,16 +2,17 @@ local null_ls = require('null-ls')
 null_ls.setup({
 	sources = {
 		null_ls.builtins.diagnostics.mypy.with({
-			extra_args = (function()
-				local venv_python = ".venv/bin/python"
-				if vim.fn.filereadable(venv_python) == 1 then
-					return { "--python-executable", venv_python }
-				elseif vim.fn.executable("python") == 1 then
-					return { "--python-executable", "python" }
-				else
-					return { "--python-executable", "python3" }
-				end
-			end)(),
+			extra_args = {'--python-executable', 'python'},
+			-- extra_args = (function()
+			-- 	local venv_python = ".venv/bin/python"
+			-- 	if vim.fn.filereadable(venv_python) == 1 then
+			-- 		return { "--python-executable", venv_python }
+			-- 	elseif vim.fn.executable("python") == 1 then
+			-- 		return { "--python-executable", "python" }
+			-- 	else
+			-- 		return { "--python-executable", "python3" }
+			-- 	end
+			-- end)(),
 
 			command = vim.fn.stdpath("data") .. "/mason/bin/mypy",
 		}),
