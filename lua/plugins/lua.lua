37af85fa55303729453a11658d5d1162979c1668
Joseph P. <joseph.pena.q@gmail.com>
2025-04-30 10:52:50 -0300

    🔧 setup lazydev for avante

--- lua/plugins/lua.lua
+++ lua/plugins/lua.lua
@@ -2,16 +2,21 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
+    cmd = "LazyDev",
     opts = {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "luvit-meta/library",                      words = { "vim%.uv" } },
+        { path = "~/projects/nvim-plugins/avante.nvim/lua", words = { "avante" } },
       },
+    },
+    dependencies = {
+      { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional cmp completion source for require statements and module annotations
+  {                                        -- optional cmp completion source for require statements and module annotations
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       opts.sources = opts.sources or {}
