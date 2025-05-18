124fee1bbde0f2deec32bdc907a133aacd196fc2
Joseph P. <joseph.pena.q@gmail.com>
2025-05-18 11:59:18 -0300

    ⚰️ remove commented background related code

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,15 +1,6 @@
 function ColorMyPencils(color)
   color = color or "gruvbox"
   vim.cmd.colorscheme(color)
-
-  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
--- if env NVIM_BG_TRANSPARENT is True, then set bg to none
--- vim.env.NVIM_BG_TRANSPARENT = "True"
--- if vim.env.NVIM_BG_TRANSPARENT == "True" then
---     vim.cmd('hi Normal ctermbg=NONE guibg=NONE')
---     vim.cmd('hi NonText ctermbg=NONE guibg=NONE')
--- end
 
 ColorMyPencils("tokyonight-night")
