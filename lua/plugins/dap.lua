5f4c436d22baa209d1299e429faaecf4a361af8c
Joseph P. <joseph.pena.q@gmail.com>
2026-05-25 15:43:43 -0300

    feat(dap): remove console widget

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -13,7 +13,79 @@ return {
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
-      dapui.setup() -- use default
+      ---@type dapui.Config
+      local local_config = {
+        wrap = false,
+        icons = { expanded = "", collapsed = "", current_frame = "" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+          watch = "w",
+        },
+        element_mappings = {},
+        expand_lines = vim.fn.has("nvim-0.7") == 1,
+        force_buffers = true,
+        layouts = {
+          {
+            -- You can change the order of elements in the sidebar
+            elements = {
+              -- Provide IDs as strings or tables with "id" and "size" keys
+              -- {
+              --   id = "scopes",
+              --   size = 0.25, -- Can be float or integer > 1
+              -- },
+              { id = "breakpoints", size = 0.3 },
+              { id = "stacks",      size = 0.3 },
+              { id = "watches",     size = 0.4 },
+            },
+            size = 40,
+            position = "left", -- Can be "left" or "right"
+          },
+          {
+            elements = {
+              { id = "repl", size = 1.0 },
+              -- { id = "console", size = 0.5},
+            },
+            size = 0.40,
+            position = "bottom", -- Can be "bottom" or "top"
+          },
+        },
+        floating = {
+          max_height = nil,
+          max_width = nil,
+          border = "single",
+          mappings = {
+            ["close"] = { "q", "<Esc>" },
+          },
+        },
+        controls = {
+          enabled = vim.fn.exists("+winbar") == 1,
+          element = "repl",
+          icons = {
+            pause = "",
+            play = "",
+            step_into = "",
+            step_over = "",
+            step_out = "",
+            step_back = "",
+            run_last = "",
+            terminate = "",
+            disconnect = "",
+          },
+        },
+        render = {
+          max_type_length = nil, -- Can be integer or nil.
+          max_value_lines = 100, -- Can be integer or nil.
+          indent = 1,
+        },
+
+      }
+      dapui.setup(local_config) -- use default
       dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
       end
